4dd6cca8ac23995685e341d03263436b3f3a16e2
endless-ops <test_endless@126.com>
2023-07-24 16:51:58 +0800

    spring boot 实战 第三章 自动配置

---
+++ spring-boot-02-autoconfig/src/main/java/com/dc18669/autoconfig/SpringBoot02AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.dc18669.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02AutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02AutoconfigApplication.class, args);
+    }
+
+}
---
+++ spring-boot-02-autoconfig/src/test/java/com/dc18669/autoconfig/SpringBoot02AutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc18669.autoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02AutoconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
