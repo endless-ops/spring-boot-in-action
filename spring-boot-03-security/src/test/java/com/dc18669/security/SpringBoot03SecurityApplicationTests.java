ffdf7de304fe98a57acb30ca2a43ac5373baa320
endless-ops <test_endless@126.com>
2023-07-29 22:08:55 +0800

    spring boot 实战 第三章 自动配置

---
+++ spring-boot-03-security/src/test/java/com/dc18669/security/SpringBoot03SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc18669.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
