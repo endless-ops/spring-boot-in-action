83c81800a33d4ddeb7b4ffb223721538ca70fe24
endless-ops <test_endless@126.com>
2023-08-28 21:02:12 +0800

    spring boot 实战 第三章 自动配置-自定义配置 Spring Security 基于 JDBC 的用户存储

--- spring-boot-demo/src/main/java/com/dc18669/demo/config/SecurityConfig.java
+++ spring-boot-demo/src/main/java/com/dc18669/demo/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.dc18669.demo.config;
 
 
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,7 +13,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,10 +29,19 @@ public class SecurityConfig {
         http.authorizeHttpRequests(
                         (auth) -> {
                             auth.requestMatchers("/css/**", "/js/**", "/images/**").permitAll();
+                            // anyRequest : 任意请求
+                            // authenticated : 指定请求需要认证身份
+                            // permitAll : 允许指定的请求进行访问
                             auth.anyRequest().authenticated();
                         }
-                ).formLogin(form -> form.loginPage("/ot/login")
+                )
+                // formLogin ： 开启表单登录
+                .formLogin(form -> form
+                        // loginPage ： 跳转指定的登录页面
+                        .loginPage("/ot/login")
+                        // loginProcessingUrl ： 提交表单接口。此值和 表单的action属性值一致。
                         .loginProcessingUrl("/tea/login")
+                        // defaultSuccessUrl ： 登陆成功后重定向的路径
                         .defaultSuccessUrl("/students/find_student", true)
                         .permitAll())
                 .csrf(
@@ -35,24 +50,54 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService () {
-        // 基于 InMemoryUserDetailsManager 类的用户存储
-        // 方式一：
-        // UserDetails user = User.builder()
-        //         .username("user") // 用户名
-        //         .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW") // 密码
-        //         .roles("USER") // 角色
-        //         .build();
+    // @Bean
+    // public UserDetailsService userDetailsService () {
+    //     // 基于 InMemoryUserDetailsManager 类的用户存储
+    //     // 方式一：
+    //     // UserDetails user = User.builder()
+    //     //         .username("user") // 用户名
+    //     //         .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW") // 密码
+    //     //         .roles("USER") // 角色
+    //     //         .build();
+    //
+    //     // 方式二：
+    //     User.UserBuilder users = User.withDefaultPasswordEncoder();
+    //     UserDetails user = users
+    //             .username("user")
+    //             .password("password")
+    //             .roles("USER")
+    //             .build();
+    //     return new InMemoryUserDetailsManager(user);
+    // }
 
-        // 方式二：
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        UserDetails user = users
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        //设置数据库连接参数
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder()
                 .username("user")
-                .password("password")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
 
 }
